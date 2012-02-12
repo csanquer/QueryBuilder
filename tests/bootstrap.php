bc9b577591ddbc250cbfa818ba9ec5796c82b37c
Charles SANQUER <charles@charles-kubuntu>
2012-02-12 20:48:38 +0100

    use Autoloader as unit tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 require_once(__DIR__.'/SQL/Test/Fixtures/PDOTestCase.php');
-require_once(__DIR__.'/../lib/SQL/Base/QueryBuilder.php');
-require_once(__DIR__.'/../lib/SQL/Base/WhereQueryBuilder.php');
-require_once(__DIR__.'/../lib/SQL/SelectQueryBuilder.php');
-require_once(__DIR__.'/../lib/SQL/UpdateQueryBuilder.php');
-require_once(__DIR__.'/../lib/SQL/InsertQueryBuilder.php');
-require_once(__DIR__.'/../lib/SQL/DeleteQueryBuilder.php');
+include_once(__DIR__.'/../lib/QueryBuilderAutoload.php');
