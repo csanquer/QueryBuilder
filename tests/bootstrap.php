7697f7732b827ad4e37d770d85c4ecbadeff6666
Charles SANQUER <charles.sanquer@spyrit.net>
2012-07-18 15:22:50 +0200

    remove composer autoload for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 require_once(dirname(__FILE__).'/SQL/Test/Fixtures/PDOTestCase.php');
-require_once(dirname(__FILE__).'/../vendor/autoload.php');
+require_once(dirname(__FILE__).'/../src/QueryBuilderLoad.php');
