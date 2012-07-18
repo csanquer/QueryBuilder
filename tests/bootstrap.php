5238e6ce5caf6c8dfb95b182087fd3138c774e4c
Charles SANQUER <charles.sanquer@spyrit.net>
2012-07-18 13:38:15 +0200

    add composer config and refactore directory tree

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 require_once(dirname(__FILE__).'/SQL/Test/Fixtures/PDOTestCase.php');
-require_once(dirname(__FILE__).'/../lib/QueryBuilderLoad.php');
+require_once(dirname(__FILE__).'/../vendor/autoload.php');
